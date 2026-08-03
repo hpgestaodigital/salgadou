23f176aaf64d985b05b2ecd2a1e3faa77a6d2859
hpgestaodigital <159062640+hpgestaodigital@users.noreply.github.com>
2026-08-03 13:56:12 -0300

    Cria seção exclusiva de integrações

---
+++ app/integracoes/page.tsx
@@ -0,0 +1,5 @@
+import { ProductionIntegrationsRoadmap } from "@/components/production-integrations-roadmap"
+
+export default function IntegracoesPage() {
+  return <ProductionIntegrationsRoadmap />
+}
